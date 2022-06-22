0f46b834b9d4894c4fc8b596ed1c29bdda9c1667
Salluri777 <saivenkata7557@gmail.com>
2022-06-22 20:45:48 +0530

    after changes

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 
 System.out.println("hello Jenkins ");
 System.out.println("what are you doing>>");
+System.out.println("After changing the java program");
 }
 }
